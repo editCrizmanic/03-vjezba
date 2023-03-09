2df14ffbe4ee7ed399743c52e0976a846907fa0c
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-03-09 19:52:03 +0100

    component that receives some data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Data from './components/Data';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Data num="f"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Data.jsx
@@ -0,0 +1,9 @@
+const Data = ({ num }) => {
+    return (
+      <div>
+        <h1>broj je {num}</h1>
+      </div>
+    );
+  };
+  
+export default Data;
